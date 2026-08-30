49cfe1dd6f7df9ae8eea9e1f3add7456d13d9b90
Teja <thejanarayanbandaru15@gmail.com>
2026-08-30 12:57:10 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq={}
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+        result=sorted(freq,key=freq.get,reverse=True)
+        return result[:k]
---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen=new HashSet<>();
+        for (int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                char num=board[r][c];
+
+                if(num == '.'){
+                    continue;
+                }
+
+                if(!seen.add(num+"in row"+r)||
+                   !seen.add(num+"in col"+c)||
+                   !seen.add(num+"in box"+(r/3)+"-"+(c/3))){
+                    return false;
+                   }
+            }
+        }
+        return true;
+    }
+}
